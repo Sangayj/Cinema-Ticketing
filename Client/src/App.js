211f8cb42afb89c3bd82b7a820b84e0a361dc397
Sangayj <sangayjamtsho904@gmail.com>
2023-04-10 11:43:35 +0600

    ok

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   //something is you 12345
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
