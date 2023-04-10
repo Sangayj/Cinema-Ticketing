760af758f131e44e662e5f30b462e972351a469e
Sangayj <sangayjamtsho904@gmail.com>
2023-04-10 10:15:14 +0600

    new changes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+       
+        
+      </header>
+      <p>
+        i love you
+
+      </p>
+    </div>
+  );
+}
+
+export default App;
