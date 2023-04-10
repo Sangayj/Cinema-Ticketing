ed7fbea6aa6d82748bc32f2e543388b0a3638491
Sangayj <sangayjamtsho904@gmail.com>
2023-04-10 11:47:15 +0600

    deleted

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,17 +1,7 @@
 import "./App.css";
 
 function App() {
-<<<<<<< HEAD
-  //something is you 12345
   return <div className="App"></div>;
-=======
-  //something is you 123456789
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
->>>>>>> origin/sherab
 }
 
 export default App;
