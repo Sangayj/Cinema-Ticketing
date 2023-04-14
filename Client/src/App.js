2d8454340c8a358159265af338e1bd2080576976
Sangayj <sangayjamtsho904@gmail.com>
2023-04-14 21:14:42 +0600

    Home

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,10 +4,12 @@ import Navbar from "./Component/Navbar";
 import Home from "./Component/Home";
 import SignIn from "./Component/SignIn";
 import Login from "./Component/Login";
+import View from "./Component/View";
 
 function App() {
   return (
     <div>
+      <View />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- Client/src/Component/Home.js
+++ Client/src/Component/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import "./Home.css";
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to our Home page</h1>
+      <div className="search-bar-container">
+        <input
+          type="text"
+          placeholder="Search for movies"
+          className="search-bar-input"
+        />
+        <button className="search-bar-button">Search </button>
+      </div>
+      <div className="allmovieslist">
+        <p>All Movies List</p>
+      </div>
     </div>
   );
 };
