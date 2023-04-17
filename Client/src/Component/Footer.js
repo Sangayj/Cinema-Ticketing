08a05f72a59d171aa89230c5fd8e5ea3f7a281a4
JangchubDorji <05210221.jnec@rub.edu.bt>
2023-04-17 11:33:31 +0600

    jad

--- Client/src/Component/Footer.js
+++ Client/src/Component/Footer.js
@@ -1,38 +1,41 @@
-import React from 'react'
-import './Footer.css'
-import { FaFacebook } from 'react-icons/fa';
-import { FaTwitter } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
+import React from "react";
+import "./Footer.css";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 export default function Footer() {
   return (
-    <div className='footer'>
-        <div className='container'>
-        <div className='row-footer'>
-            {/*column*/}
-          <div className='col'>
-
-          </div>
-            <div className='col'>
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          {/*column*/}
+          <div className="col"></div>
+          <div className="col">
             <h4>Email Address: filmassociationofbhutan2021@gmail.com</h4>
             <h4>Mobile No: 2 333 779</h4>
             <h4>Address: Thimphu,BCCI Compound Bhutan</h4>
-            <div className='social-icons'>
-                        <FaFacebook size={40} color='#FFFFFF'/>
-                        <FaTwitter size={40} color='#FFFFFF'/>
-                        <FaInstagram size={40} color='#FFFFFF'/>
-                        </div> 
+            <div className="social-icons">
+              <FaFacebook size={40} color="#FFFFFF" />
+              <FaTwitter size={40} color="#FFFFFF" />
+              <FaInstagram size={40} color="#FFFFFF" />
+            </div>
 
-            <div className='row-footer'>
-              <hr style={{ height: '0.5px', color: '#ccc', backgroundColor: '#E86A33' }} />
-            <p className='col-sm'>
-              &copy;{new Date().getFullYear()}.All Rights Reserved.Filmassociation of Bhutan
-            </p>
+            <div className="row">
+              <hr
+                style={{
+                  height: "0.5px",
+                  color: "#ccc",
+                  backgroundColor: "#E86A33",
+                }}
+              />
+              <p className="col-sm">
+                &copy;{new Date().getFullYear()}.All Rights
+                Reserved.Filmassociation of Bhutan
+              </p>
             </div>
-                       
+          </div>
         </div>
-            </div>
-           
-        </div>
+      </div>
     </div>
-  )
+  );
 }
--- Client/src/Component/Home.js
+++ Client/src/Component/Home.js
@@ -1,21 +1,121 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
 import "./Home.css";
-<<<<<<<<< Temporary merge branch 1
-=========
-import image from "./c.jpg"
-// import image1 from "./jamtsho.jpg"
->>>>>>>>> Temporary merge branch 2
+import HeroSlider, { Slide } from "hero-slider";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import image1 from "../Image/image1.jpg";
+import image2 from "../Image/image2.jpg";
+import image3 from "../Image/image3.jpg";
+import image4 from "../Image/image4.jpg";
+
 const Home = () => {
   return (
     <>
-      <div>
-        <img src={image} alt='jangchub' />
-        <div class="parent">
-        <div class="child">
-        </div>
-        <div class="child">2</div>
-        <div class="child">3</div>
-        </div>
+    <HeroSlider
+      slidingAnimation="left_to_right"
+      orientation="horizontal"
+      initialSlide={1}
+      onBeforeChange={(previousSlide, nextSlide) =>
+        console.log("onBeforeChange", previousSlide, nextSlide)
+      }
+      onChange={(nextSlide) => console.log("onChange", nextSlide)}
+      onAfterChange={(nextSlide) => console.log("onAfterChange", nextSlide)}
+      style={{
+        backgroundColor: "rgba(0, 0, 0, 0.33)",
+        marginTop: "-64px",
+      }}
+      settings={{
+        slidingDuration: 250,
+        slidingDelay: 100,
+        shouldAutoplay: true,
+        shouldDisplayButtons: true,
+        autoplayDuration: 5000,
+        height: "100vh",
+        buttonStyles: {
+          backgroundColor: "transparent",
+          border: "none",
+        },
+        buttonIconStyles: {
+          fill: "white",
+        },
+      }}
+    >
+    
+      <Slide
+        background={{
+          backgroundColor: "black",
+        }}
+      >
+        <img src={image1} alt="slide-image" />
+      </Slide>
+      <Slide
+        background={{
+          backgroundColor: "black",
+        }}
+      >
+        <img src={image2} alt="slide-image" />
+      </Slide>
+      <Slide
+        background={{
+          backgroundColor: "black",
+        }}
+      >
+        <img src={image3} alt="slide-image" />
+      </Slide>
+      <Slide
+        background={{
+          backgroundColor: "black",
+        }}
+      >
+        <img src={image4} alt="slide-image" />
+      </Slide>
+      <div
+        className="slider-button slider-button-left"
+        style={{ left: "5%" }}
+      >
+        <FaArrowLeft size={32} />
+      </div>
+      <div
+        className="slider-button slider-button-right"
+        style={{ right: "5%" }}
+      >
+        <FaArrowRight size={32} />
+      </div>  
+    </HeroSlider>
+    <div class="Heading">
+      <p>All Movies List</p>
+      <form>
+        <input type="text" placeholder="Search for any Movies" />
+        <button type="submit"><i className="fa fa-search"></i>Search</button>
+      </form>
+    </div>
+
+    <p></p>
+    <p></p>
+    <div className="parent-container">
+      <div className="child-container">Column 1, Row 1
+      <button class="view-details-btn">View Details</button>
+      </div>
+
+      <div className="child-container">Column 2, Row 1
+      <button class="view-details-btn">View Details</button>
+      </div>
+
+      <div className="child-container">Column 3, Row 1
+      <button class="view-details-btn">View Details</button>
+      </div>
+
+      <div className="child-container">Column 1, Row 2
+      <button class="view-details-btn">View Details</button>
+      </div>
+
+      <div className="child-container">
+      <img src="" alt="" />
+      <button class="view-details-btn">View Details</button>
+      </div>
+
+      <div className="child-container">Column 3, Row 2
+      <button class="view-details-btn">View Details</button>
       </div>
     </div>
     
