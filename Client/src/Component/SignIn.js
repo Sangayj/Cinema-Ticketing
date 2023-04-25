d8be41d75e50c30bac80df0d4cb8f725abf86c85
sherabloday <sheroublotay@gmail.com>
2023-04-25 20:31:50 +0600

    dfcvaf

--- Client/src/Component/SignIn.js
+++ Client/src/Component/SignIn.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-import "./Signin.css";
+import "./SignIn.css";
 
 const Signin = () => {
   const history = useNavigate();
